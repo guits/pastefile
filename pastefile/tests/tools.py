96824a82d54901d84178f2ba549776dbd78b3d29
Gael Lambert <gael.lambert@enovance.com>
2015-12-24 16:24:09 +0100

    tests/tools.py: Split write_random_file into 2 function.
    Make the function `write_file` to be used in `get` tests

--- pastefile/tests/tools.py
+++ pastefile/tests/tools.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 import os
-import pastefile.app as flaskr
 from pastefile import utils
 
 
 def write_random_file(filename):
     "Write file with a random content and return the md5"
     rnd_str = os.urandom(1024)
+    return write_file(filename=filename, content=rnd_str)
+
+def write_file(filename, content):
+    "Write file on disk and return the md5"
     with open(filename, 'w+') as f:
-        f.writelines(rnd_str)
+        f.writelines(content)
     return utils.get_md5(filename)
